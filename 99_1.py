8e8f2d642c69ba58f149a227c6522feb48b1a106
nilinswap <nilinswap@gmail.com>
2018-03-25 04:37:12 +0530

    music stopped, few 'try-catch'es added

--- 99_1.py
+++ 99_1.py
@@ -2,17 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.common.exceptions import NoSuchElementException
+import os
+#os.system("pactl set-sink-mute 0 1")
+
 def initial_url():
 	driver = webdriver.Firefox()
 	driver.get("http://991thewhale.com/listen-live/popup/")
-	el = driver.find_element_by_xpath('//*[@id="player-content"]/div/div/div/section/iframe')
+	while True:
+		try:
+			el = driver.find_element_by_xpath('//*[@id="player-content"]/div/div/div/section/iframe')
+			break;
+		except NoSuchElementException:
+			print("NoSuchElementException here for iframe link")
+			continue
+	
+	
 	p = el.get_attribute('src')
 	driver.close()
 	return p
+
+
+	
+
+
 def main_loop():
 	src = initial_url()
 	driver = webdriver.Firefox()
 	driver.get(src)
+	time.sleep(2)
 	pst = ''
 	while (1):
 		try:
@@ -52,8 +69,8 @@ def main_loop():
 		file_ob = open("songs.txt", "r+")
 		st_lines = file_ob.readlines()
 		file_ob.close()
-		print(st)
-		print(st_lines)
+		#print(st)
+		#print(st_lines)
 		print(st in st_lines)
 		if st not in st_lines:
 			file_ob = open("songs.txt", "a+")
@@ -65,6 +82,8 @@ def main_loop():
 		time.sleep(5)
 	driver.close()
 if __name__ == '__main__':
+	
+	print( os.system('sudo osascript -e "set Volume 0" shinigamilovE1'))
 	main_loop()
 
 
